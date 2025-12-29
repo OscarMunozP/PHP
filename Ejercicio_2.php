9f4108185173e1147b1460e7e0f96d626322f6ff
OscarMunozP <oscarmp200428@gmail.com>
2025-12-29 12:49:14 +0100

    Create Ejercicio_2.php

---
+++ Ejercicio_2.php
@@ -0,0 +1,50 @@
+<?php
+
+$carrito = [
+    ["producto" => "Portátil", "precio" => 1200, "cantidad" => 1],
+    ["producto" => "Ratón", "precio" => 25, "cantidad" => 2],
+    ["producto" => "Teclado", "precio" => 45, "cantidad" => 1],
+];
+
+function calcularTotal($carrito) {
+    $total = 0;
+
+    foreach ($carrito as $item) {
+        $subtotal = $item["precio"] * $item["cantidad"];
+        $total += $subtotal;
+    }
+
+    return $total;
+}
+
+foreach ($carrito as $item) {
+    $nombre = $item["producto"];
+    $precio = $item["precio"];
+    $cantidad = $item["cantidad"];
+    $subtotal = $precio * $cantidad;
+
+    echo "Producto: $nombre<br>";
+    echo "Precio unitario: $precio €<br>";
+    echo "Cantidad: $cantidad<br>";
+    echo "Subtotal: $subtotal €<br><br>";
+}
+
+$totalSinDescuento = calcularTotal($carrito);
+
+$descuento = 0;
+
+if ($totalSinDescuento > 1000) {
+    $descuento = 0.10;
+} elseif ($totalSinDescuento > 500) {
+    $descuento = 0.05;
+}
+
+$montoDescuento = $totalSinDescuento * $descuento;
+$totalFinal = $totalSinDescuento - $montoDescuento;
+
+echo "<hr>";
+echo "Total sin descuento: $totalSinDescuento €<br>";
+echo "Descuento aplicado: " . ($descuento * 100) . "%<br>";
+echo "Total final: $totalFinal €<br>";
+
+?>
